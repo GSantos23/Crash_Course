e1174e7804ca633436cff536a58c7c0089962abc
GSantos23 <santosgerson64@gmail.com>
2018-06-19 09:40:05 -0600

    Exercise 2.3

---
+++ Chapter2/Exercises/ex2_3.py
@@ -0,0 +1,7 @@
+# Exercise 2.3
+#	Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,
+#	would you like to learn some Python today?
+name = "Gerson"
+message = "Hello " + name + ", would you like to learn some python today?"
+
+print(message)
