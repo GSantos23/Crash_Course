bf576e7cfe0c86e1ff7d16e082f1b1f43c2a35d7
GSantos23 <santosgerson64@gmail.com>
2018-06-19 10:26:15 -0600

    Exercise 2.9

---
+++ Chapter2/Exercises/ex2_9.py
@@ -0,0 +1,8 @@
+# Exercise 2.9
+# Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. 
+#	Print that message.
+
+fav_num = 23
+message = "My favorite number is: "
+
+print(message + str(fav_num))
