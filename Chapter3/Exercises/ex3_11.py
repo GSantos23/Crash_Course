fa89ee5b89c4d66d79602244b1852c13b0da110b
GSantos23 <santosgerson64@gmail.com>
2018-06-19 22:42:35 -0600

    Exercise 3.11

---
+++ Chapter3/Exercises/ex3_11.py
@@ -0,0 +1,16 @@
+# Exercise 3.11
+# Intentional Error: If you haven’t received an index error in one of your
+# programs yet, try to make one happen. Change an index in one of your pro-
+# grams to produce an index error. Make sure you correct the error before clos-
+# ing the program.
+
+dinner = []
+dinner.append('Abraham Lincon')
+dinner.append('Linus Torvalds')
+dinner.append('Robert Tomasulo')
+
+print(dinner[0])
+print(dinner[1])
+print(dinner[2])
+
+# print(dinner[3])		# uncomment to see error
