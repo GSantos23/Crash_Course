b65c68f1194e4d4782a5357b0dca3c738c95e9f2
GSantos23 <santosgerson64@gmail.com>
2018-06-19 18:54:20 -0600

    Exercise 3.2

---
+++ Chapter3/Exercises/ex3_2.py
@@ -0,0 +1,12 @@
+# Exercise 3.2
+# Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name, print a message to them. 
+# The text of each message should be the same, but each message should be personalized with the person’s name.
+
+friends = ['Abigail', 'Andres', 'Oswald', 'Yose']
+
+message = "How was your day, "
+
+print(message + friends[0] + "?")
+print(message + friends[1] + "?")
+print(message + friends[2] + "?")
+print(message + friends[-1] + "?")
