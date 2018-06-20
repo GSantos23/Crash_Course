09f51ab01067538754ccd319e084ab83de46de93
GSantos23 <santosgerson64@gmail.com>
2018-06-19 19:07:05 -0600

    Exercise 3.3

---
+++ Chapter3/Exercises/ex3_3.py
@@ -0,0 +1,19 @@
+# Exercise 3.3
+# Your Own List: Think of your favorite mode of transportation, such as a
+# motorcycle or a car, and make a list that stores several examples. Use your list
+# to print a series of statements about these items, such as “I would like to own a
+# Honda motorcycle.”
+
+cars = ['Volkswagen','Honda', 'Toyota', 'Audi', 'Jeep']
+
+statement1 = "I would like to upgrade my " + cars[0] + " car."
+statement2 = "I had owned two " + cars[1] + " cars."
+statement3 = cars[2] + " is a old and trusty brand"
+statement4 = "I would like to own an " + cars[3] + "." 
+statement5 = "If one day, I own a " + cars[-1] + " ,I would like to visit a forest"
+
+print(statement1)
+print(statement2)
+print(statement3)
+print(statement4)
+print(statement5)
