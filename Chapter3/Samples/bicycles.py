f704d3cd4a86d8672c57deb0e6632194041f5ed3
GSantos23 <santosgerson64@gmail.com>
2018-06-19 18:34:35 -0600

    Sample 3.1

---
+++ Chapter3/Samples/bicycles.py
@@ -0,0 +1,9 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+# Index positions start at 0 
+print(bicycles[0])
+print(bicycles[0].title())
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
