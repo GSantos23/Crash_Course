6efff6d090e5a53422599b2b292ae76b92a7fa68
GSantos23 <santosgerson64@gmail.com>
2018-06-19 21:31:42 -0600

    Exercise 3.4

---
+++ Chapter3/Exercises/ex3_4.py
@@ -0,0 +1,14 @@
+# Exercise 3.4
+# Guest List: If you could invite anyone, living or deceased, to dinner, who
+# would you invite? Make a list that includes at least three people you’d like to
+# invite to dinner. Then use your list to print a message to each person, inviting
+# them to dinner.
+
+dinner = []
+dinner.append('Abraham Lincon')
+dinner.append('Linus Torvalds')
+dinner.append('Robert Tomasulo')
+
+print(dinner[0])
+print(dinner[1])
+print(dinner[2])
