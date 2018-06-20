9d3d480a4b8fa1a9c79ad53f70954b4e0ea58e82
GSantos23 <santosgerson64@gmail.com>
2018-06-19 22:33:25 -0600

    Exercise 3.9

---
+++ Chapter3/Exercises/ex3_9.py
@@ -0,0 +1,15 @@
+# Exercise 3.9
+# Dinner Guests: Working with one of the programs from Exercises 3-4
+# through 3-7 (page 46), use len() to print a message indicating the number
+# of people you are inviting to dinner.
+
+dinner = []
+dinner.append('Abraham Lincon')
+dinner.append('Linus Torvalds')
+dinner.append('Robert Tomasulo')
+
+print(dinner[0])
+print(dinner[1])
+print(dinner[2])
+
+print("number of people invited: " + str(len(dinner)))
