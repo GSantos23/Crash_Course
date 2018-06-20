f18b520a1a00447889f1c93c2d5555de9f22e63d
GSantos23 <santosgerson64@gmail.com>
2018-06-19 21:28:24 -0600

    Sample 3.2

---
+++ Chapter3/Samples/motorcycles.py
@@ -0,0 +1,27 @@
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+#motorcycles[0] = 'ducati'
+#print(motorcycles)
+
+
+# Append element at end of list
+#motorcycles.append('ducati')
+#print(motorcycles)
+
+# Inserting Elements into a List
+#motorcycles.insert(0, 'ducati')
+#print(motorcycles)
+
+# Removing element from list
+#del motorcycles[0]
+#print(motorcycles)
+
+# Use an element after remove
+#popped_motorcyle = motorcycles.pop()
+#print(motorcycles)
+#print(popped_motorcyle)
+
+# Another way to remove
+motorcycles.remove('ducati')
+print(motorcycles)
