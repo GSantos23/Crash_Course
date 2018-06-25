21dbb01a23a972d7d784f16dedadb6a870a71a3a
GSantos23 <santosgerson64@gmail.com>
2018-06-25 16:57:06 -0600

    Sample 4.6

---
+++ Chapter4/Samples/players.py
@@ -0,0 +1,7 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+
+print(players[0:3])
+print(players[1:4])
+print(players[:4])			# To start from begining of list
+print(players[2:])			# To print until last member
+print(players[-3:])			# From the end
