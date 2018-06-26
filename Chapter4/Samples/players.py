aa92e66c2275a1d0a9095254bad47152104d1eee
GSantos23 <santosgerson64@gmail.com>
2018-06-25 20:04:54 -0600

    Sample 4.6

--- Chapter4/Samples/players.py
+++ Chapter4/Samples/players.py
@@ -5,3 +5,7 @@ print(players[1:4])
 print(players[:4])			# To start from begining of list
 print(players[2:])			# To print until last member
 print(players[-3:])			# From the end
+
+print("Here are the first three players on my team: ")
+for player in players[:3]:
+	print(player.title())
