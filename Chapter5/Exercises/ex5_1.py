4b7bc1f0d5e6d91d64006d8c99dc52c8d1e704c1
GSantos23 <santosgerson64@gmail.com>
2018-06-26 10:08:39 -0600

    Exercise 5.1

---
+++ Chapter5/Exercises/ex5_1.py
@@ -0,0 +1,59 @@
+# Exercise 5.1
+"""
+Conditional Tests: Write a series of conditional tests. Print a statement
+describing each test and your prediction for the results of each test. 
+Your code should look something like this:
+	car = 'subaru'
+	print("Is car == 'subaru'? I predict True.")
+	print(car == 'subaru')
+	print("\nIs car == 'audi'? I predict False.")
+	print(car == 'audi')
+	• Look closely at your results, and make sure you understand why each line
+	evaluates to True or False .
+	• Create at least 10 tests. Have at least 5 tests evaluate to True and 
+	another 	5 tests evaluate to False .
+"""
+#1
+car = 'Volkswagen'
+print("Is car == 'Volkswagen'? I predict True.")
+print(car == 'Volkswagen')
+
+#2
+print("\nIs car == 'Honda'? I predict False")
+print(car == 'Honda')
+
+#3
+car = 'Honda'
+print("\n Is car == 'Honda'? I predict True")
+print( car == 'Honda')
+
+#4
+car = 'Ford'
+print("\nIs car == 'Ford'?, I predict True")
+print(car == 'Ford')
+
+#5
+print("\nIs car == 'Toyota'? I predict False")
+print(car == 'Toyota')
+
+#6
+car = 'Toyota'
+print("\nIs car == 'Toyota'? I predict True")
+print(car == 'Toyota')
+
+#7
+car = 'Kia'
+print("\nIs car == 'Kia'? I predict True")
+print(car == 'Kia')
+
+#8
+print("\nIs car == 'Chevrolet'? I predict False")
+print(car == 'Chevrolet')
+
+#9
+print("\nIs car == 'Nissan'? I predict False")
+print(car == 'Nissan')
+
+#8
+print("\nIs car == 'Tesla'? I predict False")
+print(car == 'Tesla')
