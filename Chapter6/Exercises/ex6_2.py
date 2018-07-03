bc7db8264a7542a278948d8fdbd3c586a9269e0b
GSantos23 <santosgerson64@gmail.com>
2018-07-03 12:51:01 -0600

    Exercise 6.2

---
+++ Chapter6/Exercises/ex6_2.py
@@ -0,0 +1,16 @@
+# Exercise 6.2
+"""
+6-2. Favorite Numbers: Use a dictionary to store people’s favorite numbers.
+Think of five names, and use them as keys in your dictionary. Think of a 
+favorite number for each person, and store each as a value in your dictionary. 
+Print each person’s name and their favorite number. For even more fun, poll a
+few friends and get some actual data for your program.
+"""
+
+fav_number = {'Gerson': 23, 'Yose': 56, 'Miguel': 2, 'Sarah': 99, 'Obi': 66}
+
+print("Gerson's favorite number is " + str(fav_number['Gerson']))
+print("Yose's favorite number is " + str(fav_number['Yose']))
+print("Miguel's favorite number is " + str(fav_number['Miguel']))
+print("Sarah's favorite number is " + str(fav_number['Sarah']))
+print("Obi's favorite number is " + str(fav_number['Obi']))
