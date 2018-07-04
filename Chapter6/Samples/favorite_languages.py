5ed1989d89603eea644e2fd91e18423031493950
GSantos23 <santosgerson64@gmail.com>
2018-07-03 21:50:44 -0600

    Update Sample 6.2

--- Chapter6/Samples/favorite_languages.py
+++ Chapter6/Samples/favorite_languages.py
@@ -5,6 +5,50 @@ favorite_languages = {
 	'phil': 'python'
 }
 
+"""
 print("Sarah's favorite language is " +
 	favorite_languages['sarah'].title() +
 	".")
+"""
+
+for name, language in favorite_languages.items():
+	print(name.title() + "'s favorite language is " + language.title() + ".")
+
+print()
+
+for name in favorite_languages.keys():
+	print(name.title())
+
+print()
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+	print(name.title())
+
+	if name in friends:
+		print(" Hi " + name.title() + " , I see your favorite language is " +
+		      favorite_languages[name].title() + "!")
+
+
+print()
+if 'erin' not in favorite_languages.keys():
+	print("Erin, please take our poll!")
+
+
+# Loop through dictionary in order
+for name in sorted(favorite_languages):
+	print(name.title() + ", thank you for taking the poll.")
+
+
+# Looping through all values
+print()
+print("The following languages have been mentionated:")
+for language in favorite_languages.values():
+	print(language.title())
+
+
+# Use set to prevent repetition
+print()
+print("The following languages have been mentionated:")
+for language in set(favorite_languages.values()):
+	print(language.title())
