5056a7da005c9b540f0ac40585342d6e6d2f1cc2
GSantos23 <santosgerson64@gmail.com>
2018-07-05 19:12:27 -0600

    Update Sample 6.1

--- Chapter6/Samples/alien.py
+++ Chapter6/Samples/alien.py
@@ -41,3 +41,29 @@ alien_0['x_position'] = alien_0['x_position'] + x_increment
 
 print("New x-position: " + str(alien_0['x_position']))
 
+# Nesting
+print("\nNesting")
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+	print(alien)
+
+# Make empty list for storing aliens
+aliens = []
+
+# Make 30 green aliens
+for alien_number in range(30):
+	new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+	aliens.append(new_alien)
+
+# Show the first 5 aliens
+for alien in aliens[:5]:
+	print(alien)
+print("...")
+
+# Show how many aliens have been created
+print("Total number of aliens: " + str(len(aliens)))
