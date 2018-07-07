c56ca89ae7aef1babb22a3dc87e6152e954041be
GSantos23 <santosgerson64@gmail.com>
2018-07-07 16:06:02 -0600

    Sample 7.2

---
+++ Chapter7/Samples/greeter.py
@@ -0,0 +1,7 @@
+# Sample 7.2
+prompt = "If you tell us who you are, we can personalize the message you see."
+prompt += "\nWhat is your name?"
+
+#name = input("Please enter your name: ")
+name = input(prompt)
+print("Hello, " + name + "!")
