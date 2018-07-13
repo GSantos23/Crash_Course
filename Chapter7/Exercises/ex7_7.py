885808195608ef4183749e376316d36014f84747
GSantos23 <santosgerson64@gmail.com>
2018-07-13 09:13:23 -0600

    Exercise 7.7

---
+++ Chapter7/Exercises/ex7_7.py
@@ -0,0 +1,10 @@
+# Exercise 7.7
+"""
+Infinity: Write a loop that never ends, and run it. (To end the loop, press
+ctrl - C or close the window displaying the output.)
+"""
+
+message = "Good Morning !"
+
+while True:
+	print(message)
