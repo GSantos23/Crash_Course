2d5a96b0052b9a2a1c814269f75c421299773541
GSantos23 <santosgerson64@gmail.com>
2018-07-15 19:25:21 -0600

    Sample 8.2

---
+++ Chapter8/Samples/pets.py
@@ -0,0 +1,9 @@
+# Sample 8.2
+def describe_pet(animal_type, pet_name):
+	"""Display information about a pet"""
+	print("\nI have aa " + animal_type + ".")
+	print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+
+describe_pet('hamster', 'harry')
+describe_pet('dog', 'willie') 
