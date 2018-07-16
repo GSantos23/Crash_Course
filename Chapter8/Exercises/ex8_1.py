87e5a98605f7eabe3a61953f0feba6307d755cd9
GSantos23 <santosgerson64@gmail.com>
2018-07-15 19:10:18 -0600

    Exercise 8.1

---
+++ Chapter8/Exercises/ex8_1.py
@@ -0,0 +1,12 @@
+# Exercise 8.1
+'''
+Message: Write a function called display_message() that prints one sen-
+tence telling everyone what you are learning about in this chapter. Call the
+function, and make sure the message displays correctly.
+'''
+
+def display_message():
+	"""Display learning."""
+	print("Functions and docstrings!!!")
+
+display_message()
