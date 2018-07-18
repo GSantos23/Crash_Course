2e81f1f57fb7f6b21e98ebd5297964566dcdac01
GSantos23 <santosgerson64@gmail.com>
2018-07-18 16:51:45 -0600

    Along with ex8_15.py

---
+++ Chapter8/Exercises/printing_models.py
@@ -0,0 +1,8 @@
+
+import ex8_15 as pm
+
+unprinted_designs = ['iphone case', 'robot pendant', 'dodacahedron']
+completed_models  = []
+
+pm.print_models(unprinted_designs, completed_models)
+pm.show_completed_models(completed_models)
