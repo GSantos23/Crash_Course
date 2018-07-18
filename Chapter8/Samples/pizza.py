bda8a125d64ed7a03e194d1baffd3c73068a6491
GSantos23 <santosgerson64@gmail.com>
2018-07-18 16:41:06 -0600

    Update Sample 8.7

--- Chapter8/Samples/pizza.py
+++ Chapter8/Samples/pizza.py
@@ -1,26 +1,25 @@
 # Sample 8.7
-
+'''
 def make_pizza(*toppings):
-	#"""Print the list of toppings that have been requested"""
+	#Print the list of toppings that have been requested"""
 	##print(toppings)
-	""" Summarize the pizza we are about to make."""
+	# Summarize the pizza we are about to make."""
 	print("\nMaking a pizza with the following toppings:")
 	for topping in toppings:
 		print("- " + topping)
 '''
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheeese')
-'''
+#make_pizza('pepperoni')
+#make_pizza('mushrooms', 'green peppers', 'extra cheeese')
+
 
 # Version 2
-'''
 def make_pizza(size, *toppings):
 	""" Summarize the pizza we are about to make."""
 	print("\nMaking a " + str(size) + 
 		"-inch pizza with the following toppings:")
 	for topping in toppings:
 		print("- " + topping)
-
+'''
 make_pizza(16, 'pepperoni')
 make_pizza(12, 'mushrooms', 'green peppers', 'extra cheeese')
 '''
