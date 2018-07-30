7f0249c5b3f3271b247ba3246ea31513fcce2da0
GSantos23 <santosgerson64@gmail.com>
2018-07-29 19:28:44 -0600

    Exercise 10.10

---
+++ Chapter10/Exercises/ex10_10.py
@@ -0,0 +1,42 @@
+# Exercise 10.10
+'''
+Common Words: Visit Project Gutenberg (http://gutenberg.org/ )
+and find a few texts you’d like to analyze. Download the text files for these
+works, or copy the raw text from your browser into a text file on your
+computer.
+You can use the count() method to find out how many times a word or
+phrase appears in a string. For example, the following code counts the number
+of times 'row' appears in a string:
+-----------------------------------------------------------
+>>> line = "Row, row, row your boat"
+>>> line.count('row')
+2
+>>> line.lower().count('row')
+3
+-----------------------------------------------------------
+Notice that converting the string to lowercase using lower() catches
+all appearances of the word you’re looking for, regardless of how it’s
+formatted.
+Write a program that reads the files you found at Project Gutenberg and
+determines how many times the word 'the' appears in each text.
+'''
+
+line = "The, the"
+#print(line.count('row'))
+#print(line.lower().count('row'))
+
+def read_file(file_name):
+	"""Function to read files"""
+	try:
+		with open(file_name) as f_obj:
+			contex = f_obj.read()
+	except FileNotFoundError:
+		print("File " + file_name + " does not exist. Sorry")
+	else:
+		the_count = contex.lower().count('the')
+		print("# of 'the' in " + file_name  + " >> " + str(the_count))
+
+filename = ['buster.txt', 'silverspur.txt', 'mr_standfast.txt']
+
+for archive in filename:
+	read_file(archive)
