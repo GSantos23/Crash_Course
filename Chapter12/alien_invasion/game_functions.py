45288efc20e1f30cb55922aa68ee0c4530bfc246
GSantos23 <santosgerson64@gmail.com>
2018-08-01 10:32:45 -0600

    Add keypress events

--- Chapter12/alien_invasion/game_functions.py
+++ Chapter12/alien_invasion/game_functions.py
@@ -2,11 +2,19 @@
 import sys
 import pygame
 
-def check_events():
+
+def check_events(ship):
 	"""Respond to keypresses and mouse events."""
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
+
+		elif event.type == pygame.KEYDOWN:
+			check_keydown_events(event, ship)
+		elif event.type == pygame.KEYUP:
+			check_keyup_events(event, ship)
+
+
 
 def update_screen(ai_settings, screen, ship):
 	"""Update images on the screen and flip to the new screen."""
@@ -16,3 +24,18 @@ def update_screen(ai_settings, screen, ship):
 
 	# Make the most recently draw screen visible
 	pygame.display.flip()
+
+
+def check_keydown_events(event, ship):
+	"""Respond to keypresses."""
+	if event.key == pygame.K_RIGHT:   		# Move ship to the right
+		ship.moving_right = True
+	elif event.key == pygame.K_LEFT:		# Move ship to the left
+		ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+	if event.key == pygame.K_RIGHT:
+			ship.moving_right = False
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = False
