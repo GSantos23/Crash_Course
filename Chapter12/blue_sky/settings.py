632dbd253b86128ae988f708dabe795081baa889
GSantos23 <santosgerson64@gmail.com>
2018-07-31 18:12:12 -0600

    GUI settings

---
+++ Chapter12/blue_sky/settings.py
@@ -0,0 +1,8 @@
+# Class to add GUI Features
+class Settings():
+	"""A Class to store gui settings."""
+	def __init__(self):
+		# Screen init
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (0,0,255)	# RGB format
